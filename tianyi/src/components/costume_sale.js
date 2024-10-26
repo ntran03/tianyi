da026617ec41e20c08cacbc74384048747a40829
Nicole Tran <nicoletran1234@gmail.com>
2024-10-26 15:22:12 -0400

    pages

--- tianyi/src/components/costume_sale.js
+++ tianyi/src/components/costume_sale.js
@@ -1,4 +1,11 @@
+import React from 'react';
+
 function Costumes() {
+    return (
+        <div>
+            <h1>Welcome to the Costumes Page</h1>
+        </div>
+    );
 }
 
 export default Costumes;
--- tianyi/src/components/dancers.js
+++ tianyi/src/components/dancers.js
@@ -1,3 +1,11 @@
+import React from 'react';
+
 function Dancers() {
+    return (
+        <div>
+            <h1>Welcome to the Dancers Page</h1>
+        </div>
+    );
 }
+
 export default Dancers;
