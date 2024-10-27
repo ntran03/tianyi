81b7faa52cd53b9d4ee8236186ca9aa41f1e45f0
Nicole Tran <nicoletran1234@gmail.com>
2024-10-27 00:00:29 -0400

    card

--- tianyi/src/components/home.js
+++ tianyi/src/components/home.js
@@ -35,15 +35,15 @@ function Home() {
                 </div>
             </Box>
             {/* Grid for two cards */}
-            <Grid container spacing={4} sx={{ mb: 2 }}> {/* Increased spacing */}
+            <Grid container spacing={4} sx={{ mb: 2, px: 2 }}>
                 <Grid item xs={12} sm={6}>
                     <Card 
                         variant="outlined" 
-                        sx={{ padding: 3, textAlign: 'center' }} // Increased padding
+                        sx={{ padding: 3, textAlign: 'center' }}
                     >
                         <CardContent>
                             <Typography variant="h6">
-                                Card 1
+                                Upcoming Events
                             </Typography>
                             <Typography variant="body2">
                                 This is the content of Card 1.
@@ -54,7 +54,7 @@ function Home() {
                 <Grid item xs={12} sm={6}>
                     <Card 
                         variant="outlined" 
-                        sx={{ padding: 3, textAlign: 'center' }} // Increased padding
+                        sx={{ padding: 3, textAlign: 'center'}} // Increased padding
                     >
                         <CardContent>
                             <Typography variant="h6">
@@ -67,6 +67,13 @@ function Home() {
                     </Card>
                 </Grid>
             </Grid>
+            <Card variant="elevation" sx={{ padding: 3, textAlign: 'center' }}>
+                <CardContent>
+                    <Typography variant='h6'>
+                    Tianyi Dance Team, University of Maryland's only Chinese cultural dance team, is a group of students who express interest in Chinese culture through folk and traditional Chinese dance. Established in 2012, Tianyi Dance Team strives to break down cross-cultural barriers through the graceful, yet powerful language of dance. Our team aims to widen the perspectives of the University of Maryland's student population and local communities so that we all can learn to understand and embrace different cultures. Tianyi Dance Team is committed to its responsibilities under University of Maryland, College Park Office of Campus Programs and University Recreation & Wellness policies.
+                    </Typography>
+                </CardContent>
+            </Card>
         </Box>
     );
 }
